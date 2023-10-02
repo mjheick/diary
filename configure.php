d0bfb4824b946c88597d8c9f0f1f037d35baa9e7
Matthew Heick <github@unliterate.net>
2023-10-02 23:53:40 +0000

    starting to implement timezone manipulation

--- configure.php
+++ configure.php
@@ -3,6 +3,7 @@
  * Master configuration
  *
  * 'mysql': settings to connect to mysql
+ * 'diary_timezone': What timezone to display the diary in per https://www.php.net/manual/en/timezones.php
  * 'password_to_post': Every new/edit/delete must provide this password. null disabled.
  * 'diary_days_per_page': When viewing diary how many days are rendered per page. 0 dumps the entire thing.
  * 'title': What to show at the top of every page.
@@ -15,6 +16,7 @@ $config = [
 		'database' => 'diary',
 		'table' => 'diary',
 	],
+	'diary_timezone' => 'America/New_York',
 	'password_to_post' => 'derp',
 	'diary_days_per_page' => 0,
 	'title' => 'I Envy You, Alan Rickman / Diary of Matthew Heick',
